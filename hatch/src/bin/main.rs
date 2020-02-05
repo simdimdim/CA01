3964d74d6c7e58cd2a786f5a0f365812289af90f
simdimdim <simdimdim@gmail.com>
2020-02-05 04:15:43 +0200

    vulkano updates

--- hatch/src/bin/main.rs
+++ hatch/src/bin/main.rs
@@ -1,5 +1,37 @@
 use engine::Engine;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+};
 fn main() {
-    let engine = Engine::<f32>::new();
-    engine.run();
+    let event_loop = EventLoop::new();
+    let mut engine = Engine::<f32>::new(&event_loop);
+    let mut recreate_swapchain = false;
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(_),
+            ..
+        } => {
+            recreate_swapchain = true;
+        }
+        Event::WindowEvent {
+            event: WindowEvent::CursorMoved { position, .. },
+            ..
+        } => {
+            if [position.x, position.y] != [0; 2] {
+                engine.setmouse([position.x.into(), position.y.into()])
+            }
+        }
+        Event::RedrawEventsCleared => {
+            //TODO: HiDPI scaling as push constants
+            engine.run(recreate_swapchain);
+        }
+        _ => (),
+    });
 }
